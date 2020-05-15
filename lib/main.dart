b79634b8f079a9b806a38ddce55def9ee6288cbb
SharjeelDev <61178058+SharjeelDev@users.noreply.github.com>
2020-05-16 02:14:25 +0500

    👌 IMPROVE: Random Dice

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,14 @@ class DicePage extends StatefulWidget {
 class _DicePageState extends State<DicePage> {
   int leftDiceNumber = 1;
   int rightDiceNumber = 1;
-  
+
+  void changeDiceFace() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -32,20 +39,16 @@ class _DicePageState extends State<DicePage> {
         children: <Widget>[
           Expanded(
             child: FlatButton(
-              onPressed: (){
-                setState(() {
-                  leftDiceNumber = Random().nextInt(6) + 1;
-                });
+              onPressed: () {
+                changeDiceFace();
               },
               child: Image.asset('images/dice$leftDiceNumber.png'),
             ),
           ),
           Expanded(
             child: FlatButton(
-              onPressed: (){
-                setState(() {
-                  rightDiceNumber = Random().nextInt(6) + 1;
-                });
+              onPressed: () {
+                changeDiceFace();
               },
               child: Image.asset('images/dice$rightDiceNumber.png'),
             ),
