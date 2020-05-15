ab20a937cbfebb67a30c1e0e23366d56155ba1f7
SharjeelDev <61178058+SharjeelDev@users.noreply.github.com>
2020-05-16 01:49:59 +0500

    📦 NEW: Randomising the Dice

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,7 +16,15 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+  
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -24,17 +33,21 @@ class DicePage extends StatelessWidget {
           Expanded(
             child: FlatButton(
               onPressed: (){
-                print('Left button got pressed');
+                setState(() {
+                  leftDiceNumber = Random().nextInt(6) + 1;
+                });
               },
-              child: Image.asset('images/dice1.png'),
+              child: Image.asset('images/dice$leftDiceNumber.png'),
             ),
           ),
           Expanded(
             child: FlatButton(
               onPressed: (){
-                print('Right button got pressed');
+                setState(() {
+                  rightDiceNumber = Random().nextInt(6) + 1;
+                });
               },
-              child: Image.asset('images/dice1.png'),
+              child: Image.asset('images/dice$rightDiceNumber.png'),
             ),
           ),
         ],
